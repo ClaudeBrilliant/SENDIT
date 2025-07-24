cf2f1c25652f6eb88c21a3acc8d465885801a0f8
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-07-24 12:54:49 +0300

    [modified]: parcel module

--- backend/src/parcels/parcels.module.ts
+++ backend/src/parcels/parcels.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ParcelsService } from './parcels.service';
 import { ParcelsController } from './parcels.controller';
+import { MailerModule } from '../shared/utils/mailer/mailer.module';
 
 @Module({
+  imports: [MailerModule],
   providers: [ParcelsService],
   controllers: [ParcelsController],
   exports: [ParcelsService],
