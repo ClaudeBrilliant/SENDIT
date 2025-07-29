e091b664dec114f35bdda5c555541373a333e281
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-07-29 14:39:38 -0400

    [modified]: added parcels module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
@@ -9,7 +10,17 @@ import { AuthModule } from './auth/auth.module';
 import { NotificationModule } from './notifications/notification.module';
 
 @Module({
-  imports: [AdminModule, ParcelsModule, UsersModule, CourierModule, AuthModule, NotificationModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AdminModule, 
+    ParcelsModule, 
+    UsersModule, 
+    CourierModule, 
+    AuthModule, 
+    NotificationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
